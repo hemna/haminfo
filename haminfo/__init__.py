95673d65d949a8ce2cebdecd6775619b14c39934
Hemna <waboring@hemna.com>
2021-08-10 08:47:16 -0400

    Fixed version in init

--- haminfo/__init__.py
+++ haminfo/__init__.py
@@ -5,6 +5,6 @@ import pbr.version
 __author__ = """haminfo"""
 __email__ = 'waboring@hemna.com'
 
-__version__ = pbr.version.VersionInfo("aprsd").version_string()
+__version__ = pbr.version.VersionInfo("haminfo").version_string()
 if not __version__:
     __version__ = '0.1.0'
