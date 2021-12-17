1ace23a81a45ba9fff282dfac0634ca2755d8842
Hemna <waboring@hemna.com>
2021-12-17 20:23:41 +0000

    updated build

--- docker/build.sh
+++ docker/build.sh
@@ -15,13 +15,17 @@ EOF
 
 ALL_PLATFORMS=0
 DEV=0
-TAG="master"
+TAG="latest"
+BRANCH="master"
 
-while getopts “t:da” OPTION
+while getopts “t:dab:” OPTION
 do
     case $OPTION in
         t)
            TAG=$OPTARG
+           ;;
+        b)
+           BRANCH=$OPTARG
            ;;
         a)
            ALL_PLATFORMS=1
@@ -36,7 +40,7 @@ do
     esac
 done
 
-VERSION="1.6.0"
+VERSION="2.3.1"
 
 if [ $ALL_PLATFORMS -eq 1 ]
 then
@@ -45,20 +49,28 @@ else
     PLATFORMS="linux/amd64"
 fi
 
+echo "Build with tag=${TAG} BRANCH=${BRANCH} dev?=${DEV} platforms?=${PLATFORMS}"
+
+
+echo "Destroying old multiarch build container"
+docker buildx rm multiarch
+echo "Creating new buildx container"
+docker buildx create --name multiarch --platform linux/arm/v7,linux/arm/v6,linux/arm64,linux/amd64 --config ./buildkit.toml --use --driver-opt image=moby/buildkit:master
+
 if [ $DEV -eq 1 ]
 then
+    echo "Build -DEV- with tag=${TAG} BRANCH=${BRANCH} platforms?=${PLATFORMS}"
     # Use this script to locally build the docker image
     docker buildx build --push --platform $PLATFORMS \
-        -t harbor.hemna.com/hemna6969/aprsd:$TAG \
-        -f Dockerfile-dev --no-cache .
+        -t harbor.hemna.com/hemna6969/haminfo:$TAG \
+        -f Dockerfile-dev --build-arg branch=$BRANCH --no-cache .
 else
     # Use this script to locally build the docker image
-    docker buildx build --push --platform $PLATFORMS \
-        -t hemna6969/aprsd:$VERSION \
-        -t hemna6969/aprsd:latest \
-        -t harbor.hemna.com/hemna6969/aprsd:latest \
-        -t harbor.hemna.com/hemna6969/aprsd:$VERSION \
-        -f Dockerfile .
-
-
+        #-t hemna6969/haminfo:$VERSION \
+        #-t hemna6969/haminfo:$TAG \
+    echo "Build with tag=${TAG} BRANCH=${BRANCH} platforms?=${PLATFORMS}"
+    docker buildx build --platform $PLATFORMS \
+        -t harbor.hemna.com/hemna6969/haminfo:$TAG \
+        -t harbor.hemna.com/hemna6969/haminfo:$VERSION \
+        -f Dockerfile-dev --build-arg branch=$BRANCH --no-cache --push .
 fi
