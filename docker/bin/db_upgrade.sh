1f9e8ef9a2337a906fbfd7260dff8a842f4437fa
Hemna <waboring@hemna.com>
2023-04-14 14:52:56 -0400

    Add db_upgrade.sh

---
+++ docker/bin/db_upgrade.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# The main entry point of the API service
+
+set -x
+
+COLUMNS=${COLUMNS:-160}
+export COLUMNS
+
+# TODO(walt): setup a cron to run haminfo_load
+# once a day.
+INIT_DB=${INIT_DB:-False}
+
+# check to see if there is a config file
+APP_CONFIG="/config/haminfo.conf"
+SRC_DIR="~/.local/lib/python3.9/site-packages/haminfo/db"
+DB_CONFIG="/config/alembic.ini"
+if [ ! -e "$APP_CONFIG" ]; then
+    echo "'$APP_CONFIG' File does not exist. Creating."
+    oslo-config-generator --namespace haminfo --namespace haminfo.db --namespace haminfo.flask > $APP_CONFIG
+    echo "Must configure Database Connection.  Edit $APP_CONFIG"
+else
+    cd $SRC_DIR
+    ~/.local/bin/alembic --config $DB_CONFIG upgrade head
+fi
