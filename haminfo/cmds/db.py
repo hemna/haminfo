7df9f0a241e6a9c467b9e8244e34377e437d9e6a
Walter Boring <waboring@hemna.com>
2025-06-13 16:21:33 -0400

    Fixed naming of imports

--- haminfo/cmds/db.py
+++ haminfo/cmds/db.py
@@ -6,7 +6,8 @@ import haminfo
 from haminfo.main import cli
 from haminfo import cli_helper
 from haminfo import utils
-from haminfo.db import db, migrate
+from haminfo.db import db as haminfo_db
+from haminfo.db import migrate
 
 
 CONF = cfg.CONF
@@ -29,8 +30,8 @@ def schema_init(ctx):
     """Initialize the database schema"""
     LOG.info("haminfo_load version: {}".format(haminfo.__version__))
 
-    engine = db._setup_connection()
-    db.init_db_schema(engine)
+    engine = haminfo_db._setup_connection()
+    haminfo_db.init_db_schema(engine)
 
 
 @db.command()
@@ -71,6 +72,6 @@ def clean_wx_reports(ctx):
     """Clean out old weather reports"""
     LOG.info("haminfo_load version: {}".format(haminfo.__version__))
 
-    db_session = db.setup_session()
+    db_session = haminfo_db.setup_session()
     session = db_session()
-    db.clean_weather_reports(session)
+    haminfo_db.clean_weather_reports(session)
