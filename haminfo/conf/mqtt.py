45a8332fb807bea33cf0b4485811b4a66bc81a97
Walter Boring <waboring@hemna.com>
2026-03-27 11:24:26 -0400

    feat(mqtt): add processor_count config option
    Adds configurable number of parallel APRS packet processor threads.
    Default is 4, range 1-32.
    
    Part of: parallel MQTT consumers feature

--- haminfo/conf/mqtt.py
+++ haminfo/conf/mqtt.py
@@ -2,33 +2,34 @@ from oslo_config import cfg
 
 
 mqtt_group = cfg.OptGroup(
-    name="mqtt",
-    title="MQTT Options for injesting wx packets from aprsd",
+    name='mqtt',
+    title='MQTT Options for injesting wx packets from aprsd',
 )
 
 
 mqtt_opts = [
-    cfg.StrOpt('host_ip',
-               default='0.0.0.0',
-               help='The hostname/ip address to listen on'
-               ),
-    cfg.IntOpt('host_port',
-               default=80,
-               help='The port to listen on for requests'
-               ),
-    cfg.StrOpt('user',
-               default=None,
-               help='The mqtt username to use'
-               ),
-    cfg.StrOpt('password',
-               default=None,
-               help="the mqtt password"),
-    cfg.StrOpt('topic',
-               default="aprs/weather",
-               help="The MQTT Topic to subscribe for messages"),
-    cfg.StrOpt('keepalive_file',
-               default='/tmp/haminfo_mqtt_keepalive.json',
-               help="The keepalive thread's file to update.")
+    cfg.StrOpt(
+        'host_ip', default='0.0.0.0', help='The hostname/ip address to listen on'
+    ),
+    cfg.IntOpt('host_port', default=80, help='The port to listen on for requests'),
+    cfg.StrOpt('user', default=None, help='The mqtt username to use'),
+    cfg.StrOpt('password', default=None, help='the mqtt password'),
+    cfg.StrOpt(
+        'topic', default='aprs/weather', help='The MQTT Topic to subscribe for messages'
+    ),
+    cfg.StrOpt(
+        'keepalive_file',
+        default='/tmp/haminfo_mqtt_keepalive.json',
+        help="The keepalive thread's file to update.",
+    ),
+    cfg.IntOpt(
+        'processor_count',
+        default=4,
+        min=1,
+        max=32,
+        help='Number of parallel APRS packet processor threads. '
+        'Increase to improve throughput on multi-core systems.',
+    ),
 ]
 
 
@@ -37,6 +38,4 @@ def register_opts(config):
 
 
 def list_opts():
-    return {
-        mqtt_group.name: mqtt_opts
-    }
+    return {mqtt_group.name: mqtt_opts}
