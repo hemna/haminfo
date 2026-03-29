80ae5807c8286d140119c38cbcff547c7a251de9
Walter Boring <waboring@hemna.com>
2026-03-28 21:46:55 -0400

    Add missing state/province bounding box functions to utils.py
    These functions were lost during the rebase conflict resolution:
    - US_STATE_BOUNDS, CA_PROVINCE_BOUNDS, AU_STATE_BOUNDS dictionaries
    - get_state_from_coords() - Get state/province from coordinates
    - get_states_for_country() - Get list of states for a country

--- haminfo-dashboard/src/haminfo_dashboard/utils.py
+++ haminfo-dashboard/src/haminfo_dashboard/utils.py
@@ -228,3 +228,140 @@ def _clean_comment(comment: str) -> str:
         return ''
 
     return comment
+
+
+# US state bounding boxes (min_lat, max_lat, min_lon, max_lon)
+US_STATE_BOUNDS = {
+    'AL': ('Alabama', 30.2, 35.0, -88.5, -84.9),
+    'AK': ('Alaska', 51.2, 71.4, -179.1, -130.0),
+    'AZ': ('Arizona', 31.3, 37.0, -114.8, -109.0),
+    'AR': ('Arkansas', 33.0, 36.5, -94.6, -89.6),
+    'CA': ('California', 32.5, 42.0, -124.4, -114.1),
+    'CO': ('Colorado', 37.0, 41.0, -109.1, -102.0),
+    'CT': ('Connecticut', 41.0, 42.1, -73.7, -71.8),
+    'DE': ('Delaware', 38.5, 39.8, -75.8, -75.0),
+    'FL': ('Florida', 24.5, 31.0, -87.6, -80.0),
+    'GA': ('Georgia', 30.4, 35.0, -85.6, -80.8),
+    'HI': ('Hawaii', 18.9, 22.2, -160.2, -154.8),
+    'ID': ('Idaho', 42.0, 49.0, -117.2, -111.0),
+    'IL': ('Illinois', 37.0, 42.5, -91.5, -87.5),
+    'IN': ('Indiana', 37.8, 41.8, -88.1, -84.8),
+    'IA': ('Iowa', 40.4, 43.5, -96.6, -90.1),
+    'KS': ('Kansas', 37.0, 40.0, -102.1, -94.6),
+    'KY': ('Kentucky', 36.5, 39.1, -89.6, -82.0),
+    'LA': ('Louisiana', 29.0, 33.0, -94.0, -89.0),
+    'ME': ('Maine', 43.1, 47.5, -71.1, -67.0),
+    'MD': ('Maryland', 37.9, 39.7, -79.5, -75.0),
+    'MA': ('Massachusetts', 41.2, 42.9, -73.5, -70.0),
+    'MI': ('Michigan', 41.7, 48.2, -90.4, -82.4),
+    'MN': ('Minnesota', 43.5, 49.4, -97.2, -89.5),
+    'MS': ('Mississippi', 30.2, 35.0, -91.7, -88.1),
+    'MO': ('Missouri', 36.0, 40.6, -95.8, -89.1),
+    'MT': ('Montana', 44.4, 49.0, -116.0, -104.0),
+    'NE': ('Nebraska', 40.0, 43.0, -104.1, -95.3),
+    'NV': ('Nevada', 35.0, 42.0, -120.0, -114.0),
+    'NH': ('New Hampshire', 42.7, 45.3, -72.6, -70.7),
+    'NJ': ('New Jersey', 38.9, 41.4, -75.6, -73.9),
+    'NM': ('New Mexico', 31.3, 37.0, -109.1, -103.0),
+    'NY': ('New York', 40.5, 45.0, -79.8, -71.9),
+    'NC': ('North Carolina', 33.8, 36.6, -84.3, -75.5),
+    'ND': ('North Dakota', 45.9, 49.0, -104.0, -96.6),
+    'OH': ('Ohio', 38.4, 42.0, -84.8, -80.5),
+    'OK': ('Oklahoma', 33.6, 37.0, -103.0, -94.4),
+    'OR': ('Oregon', 42.0, 46.3, -124.6, -116.5),
+    'PA': ('Pennsylvania', 39.7, 42.3, -80.5, -75.0),
+    'RI': ('Rhode Island', 41.1, 42.0, -71.9, -71.1),
+    'SC': ('South Carolina', 32.0, 35.2, -83.4, -78.5),
+    'SD': ('South Dakota', 42.5, 46.0, -104.1, -96.4),
+    'TN': ('Tennessee', 35.0, 36.7, -90.3, -81.6),
+    'TX': ('Texas', 25.8, 36.5, -106.6, -93.5),
+    'UT': ('Utah', 37.0, 42.0, -114.1, -109.0),
+    'VT': ('Vermont', 42.7, 45.0, -73.4, -71.5),
+    'VA': ('Virginia', 36.5, 39.5, -83.7, -75.2),
+    'WA': ('Washington', 45.5, 49.0, -124.8, -116.9),
+    'WV': ('West Virginia', 37.2, 40.6, -82.6, -77.7),
+    'WI': ('Wisconsin', 42.5, 47.1, -92.9, -86.8),
+    'WY': ('Wyoming', 41.0, 45.0, -111.1, -104.1),
+    'DC': ('District of Columbia', 38.8, 39.0, -77.1, -76.9),
+}
+
+# Canadian province bounding boxes
+CA_PROVINCE_BOUNDS = {
+    'AB': ('Alberta', 49.0, 60.0, -120.0, -110.0),
+    'BC': ('British Columbia', 48.3, 60.0, -139.1, -114.0),
+    'MB': ('Manitoba', 49.0, 60.0, -102.0, -89.0),
+    'NB': ('New Brunswick', 44.6, 48.1, -69.1, -63.8),
+    'NL': ('Newfoundland and Labrador', 46.6, 60.4, -67.8, -52.6),
+    'NS': ('Nova Scotia', 43.4, 47.0, -66.4, -59.7),
+    'NT': ('Northwest Territories', 60.0, 78.8, -136.5, -102.0),
+    'NU': ('Nunavut', 51.7, 83.1, -120.7, -61.2),
+    'ON': ('Ontario', 41.7, 56.9, -95.2, -74.3),
+    'PE': ('Prince Edward Island', 45.9, 47.1, -64.4, -62.0),
+    'QC': ('Quebec', 45.0, 62.6, -79.8, -57.1),
+    'SK': ('Saskatchewan', 49.0, 60.0, -110.0, -101.4),
+    'YT': ('Yukon', 60.0, 69.6, -141.0, -123.8),
+}
+
+# Australian state bounding boxes
+AU_STATE_BOUNDS = {
+    'NSW': ('New South Wales', -37.5, -28.2, 141.0, 153.6),
+    'VIC': ('Victoria', -39.2, -34.0, 140.9, 150.0),
+    'QLD': ('Queensland', -29.2, -10.7, 138.0, 153.6),
+    'SA': ('South Australia', -38.1, -26.0, 129.0, 141.0),
+    'WA': ('Western Australia', -35.1, -13.7, 112.9, 129.0),
+    'TAS': ('Tasmania', -43.6, -39.6, 143.8, 148.5),
+    'NT': ('Northern Territory', -26.0, -10.9, 129.0, 138.0),
+    'ACT': ('Australian Capital Territory', -35.9, -35.1, 148.8, 149.4),
+}
+
+
+def get_state_from_coords(
+    lat: float, lon: float, country_code: str
+) -> tuple[str, str] | None:
+    """Get state/province from coordinates for supported countries.
+
+    Args:
+        lat: Latitude
+        lon: Longitude
+        country_code: ISO country code (US, CA, AU)
+
+    Returns:
+        Tuple of (state_code, state_name) or None if not found
+    """
+    if country_code == 'US':
+        bounds = US_STATE_BOUNDS
+    elif country_code == 'CA':
+        bounds = CA_PROVINCE_BOUNDS
+    elif country_code == 'AU':
+        bounds = AU_STATE_BOUNDS
+    else:
+        return None
+
+    for code, (name, min_lat, max_lat, min_lon, max_lon) in bounds.items():
+        if min_lat <= lat <= max_lat and min_lon <= lon <= max_lon:
+            return (code, name)
+
+    return None
+
+
+def get_states_for_country(country_code: str) -> list[tuple[str, str]]:
+    """Get list of states/provinces for a country.
+
+    Args:
+        country_code: ISO country code (US, CA, AU)
+
+    Returns:
+        List of (state_code, state_name) tuples, sorted by name
+    """
+    if country_code == 'US':
+        bounds = US_STATE_BOUNDS
+    elif country_code == 'CA':
+        bounds = CA_PROVINCE_BOUNDS
+    elif country_code == 'AU':
+        bounds = AU_STATE_BOUNDS
+    else:
+        return []
+
+    return sorted(
+        [(code, name) for code, (name, *_) in bounds.items()], key=lambda x: x[1]
+    )
